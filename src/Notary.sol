d93c41acbc549c11868729d1a90e2cebff587c89
etienne-napoleone <etienne.napoleone@gmail.com>
2020-06-04 00:58:43 +0900

    feat(notray): add contract

---
+++ src/Notary.sol
@@ -0,0 +1,33 @@
+pragma solidity ^0.4.26;
+
+
+contract Notary {
+    mapping(bytes32 => address) private signer;
+
+    constructor() public payable {}
+
+    /**
+     * @dev fallback function, refusing any funds
+     */
+    function() external payable {
+        revert("Not accepting payments");
+    }
+
+    /**
+     * @dev sign a hash with the address of the sender
+     * @param _hash hash to verify (md5 hash bytes in base64)
+     */
+    function sign(bytes32 _hash) external {
+        require(signer[_hash] == address(0), "Hash already signed");
+        signer[_hash] = msg.sender;
+    }
+
+    /**
+     * @dev verify if the hash has been signed by someone
+     * @param _hash hash to verify (md5 hash bytes in base64)
+     * @return _signer address
+     */
+    function verify(bytes32 _hash) external view returns (address _signer) {
+        return signer[_hash];
+    }
+}
