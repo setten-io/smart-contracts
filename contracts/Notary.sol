3cc17bad18a9bf9b7d407c43ac109e96b3a2f573
etienne-napoleone <etienne.napoleone@gmail.com>
2020-06-21 02:19:38 +0900

    feat(notary): add fees

--- contracts/Notary.sol
+++ contracts/Notary.sol
@@ -1,16 +1,38 @@
 pragma solidity ^0.4.26;
 
+/***********************
+ *       OWNABLE       *
+ ***********************/
+contract Ownable {
+    address public owner;
+
+    constructor() public {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        if (msg.sender == owner) _;
+    }
+
+    function transferOwnership(address _newOwner) external onlyOwner {
+        if (_newOwner != address(0)) owner = _newOwner;
+    }
+}
 
 /***********************
- *                     *
  *      setten.io      *
- *                     *
+ *       NOTARY        *
  ***********************/
-
-contract Notary {
+contract Notary is Ownable {
+    uint256 public fee;
     mapping(bytes32 => address) private signer;
 
-    constructor() public payable {}
+    /**
+     * @param _fee price to pay for a signature in wei
+     */
+    constructor(uint256 _fee) public {
+        fee = _fee;
+    }
 
     /**
      * @dev fallback function, refusing any funds
@@ -20,11 +42,27 @@ contract Notary {
     }
 
     /**
+     * @dev change the siginig fee
+     * @param _fee price to pay for a signature in wei
+     */
+    function updateFee(uint256 _fee) external onlyOwner {
+        fee = _fee;
+    }
+
+    /**
+     * @dev withdraw signing fees
+     */
+    function withdraw() external onlyOwner {
+        msg.sender.transfer(address(this).balance);
+    }
+
+    /**
      * @dev sign a hash with the address of the sender
      * @param _hash hash to verify (md5 hash bytes in base64)
      */
-    function sign(bytes32 _hash) external {
+    function sign(bytes32 _hash) external payable {
         require(signer[_hash] == address(0), "Hash already signed");
+        require(msg.value >= fee, "Not enough fee");
         signer[_hash] = msg.sender;
     }
 
--- migrations/2_notary.js
+++
@@ -1,5 +0,0 @@
-const Migrations = artifacts.require("Notary");
-
-module.exports = function (deployer) {
-    deployer.deploy(Migrations);
-};
